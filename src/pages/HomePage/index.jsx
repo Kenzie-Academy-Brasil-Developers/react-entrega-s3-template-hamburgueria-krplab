de7d56be5518196523da40ff60ded4fb34874edf
krplab <kauan.r.paulino@gmail.com>
2024-01-02 22:21:50 -0300

    homepage com lógica implementada e componentes importados

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,25 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
+import { api } from "../../services/api";
+import { ButtonsCategory } from "../../components/ButtonsCategory";
 
 export const HomePage = () => {
+   const [loading, setLoading] = useState(false);
+   const [isOpenCart, setIsOpenCart] = useState(false);
+   const [itemSearch, setItemSearch] = useState("");
+   const [category, setCategory] = useState("");
    const [productList, setProductList] = useState([]);
    const [cartList, setCartList] = useState([]);
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
-   // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
+   useEffect(() => {
+      const getProducts = async () => {
+         try {
+            setLoading(true);
+            const { data } = await api.get('products', {
+               params: {
+                  category: category !== '' ? category : undefined,
+               },
+            });
+
+            const lowercaseSearch = itemSearch.toLowerCase();
+            const filteredProducts = data.filter((product) =>
+               product.name.toLowerCase().includes(lowercaseSearch)
+            );
+
+            setProductList(filteredProducts);
+         } catch (error) {
+            return <p>Erro ao atualizar cardápio</p>;
+         } finally {
+            setLoading(false);
+         }
+      };
+
+      getProducts();
+   }, [category, itemSearch]);
+
+   useEffect(() => {
+      const storedCart = JSON.parse(localStorage.getItem("@CARTPRODUCTS")) || [];
+      setCartList(storedCart);
+   }, []);
 
    return (
       <>
-         <Header />
+         <Header setIsOpenCart={setIsOpenCart} cartList={cartList} search={setItemSearch} />
          <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
+            <ButtonsCategory setCategory={setCategory} category={category} />
+            {loading ? (
+               <p>Carregando..</p>
+            ) : (
+               <ProductList productList={productList} setCartList={setCartList} cartList={cartList} />
+            )}
+
+            {isOpenCart ? <CartModal cartList={cartList} setIsOpenCart={setIsOpenCart} setCartList={setCartList} /> : null}
          </main>
       </>
    );
