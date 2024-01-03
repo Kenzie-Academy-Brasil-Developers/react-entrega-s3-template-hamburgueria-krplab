1c0f6f1aa4eac898eaa07fe1e67be754e5f0e7f2
krplab <kauan.r.paulino@gmail.com>
2024-01-02 22:23:49 -0300

    correções no arquivo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/reset.scss";
+import "./styles/globalStyles.scss";
+import "./styles/index.scss";
+import "./styles/buttons.scss";
+import "./styles/texts.scss";
+import "./styles/inputs.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
